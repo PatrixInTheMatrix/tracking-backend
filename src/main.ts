38d9cabcadf3a05951795ac2b2e769501dd1d8e5
PatrixInTheMatrix <p.perrin@diwidi.net>
2025-06-30 11:10:21 +0200

    chore: enable CORS for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// main.ts
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: [
+      'https://diwidi.net',
+      'https://www.diwidi.net',
+      'https://dev.diwidi.net',
+      'http://localhost:4200',
+    ],
+    methods: ['POST'],
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+
