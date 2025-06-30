e62e704b97c682af5e374674e0781b5254ad02f4
PatrixInTheMatrix <p.perrin@diwidi.net>
2025-06-30 19:35:24 +0200

    add sessionId to TrackingEventSchema

--- src/tracking/tracking-event.schema.ts
+++ src/tracking/tracking-event.schema.ts
@@ -4,4 +4,5 @@ export const TrackingEventSchema = new Schema({
   eventName: { type: String, required: true },
   data: { type: Schema.Types.Mixed },
   timestamp: { type: Date, default: Date.now },
+  sessionId: { type: String }
 });
